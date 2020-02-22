7b74aae465b350d8b91ad17148d02d93a733dd09
Tomas <tomas.huzevka@outlook.com>
2020-02-22 13:05:34 -0500

    bank Account

--- assignment_user/follow_on.py
+++ assignment_user/follow_on.py
@@ -21,12 +21,50 @@ class User:
         print(self.name , self.account_balance)
         return self
 
-tomas = User('Tomas', 'email')
-kana = User('Kana', 'email1')
-graham = User('Gram', 'email2')
 
-tomas.make_deposit(1000000).make_deposit(1000000).make_deposit(1000000).make_withdrawal(1).display_account_balance()
+class bank_account:
+    def __init__(self, account_name, balance, int_rate):
+        self.account_name = account_name
+        self.balance = balance
+        self.int_rate = 0.01
 
-kana.make_deposit(1000000).make_deposit(1000000).make_withdrawal(1).make_withdrawal(1).display_account_balance()
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+    def display_account_info(self):
+        print(self.account_name, self.balance, self.int_rate)
+        return self
+    def yield_interest(self):
+        self.balance *= self.int_rate
+        return self
 
-graham.make_deposit(1000000).make_deposit(1000000).make_withdrawal(1).make_withdrawal(1).display_account_balance()
+tomas_bankaccount = bank_account('richie', 1000000, 0.01)
+
+kana_bankaccount = bank_account('bankie', 1000000, 0.01)
+
+kana_bankaccount.deposit(1).deposit(1).deposit(1).withdraw(1).display_account_info()
+
+kana_bankaccount.deposit(1).deposit(1).deposit(1).withdraw(1).display_account_info()
+
+tomas_bankaccount.deposit(1).deposit(1).withdraw(1).withdraw(1).withdraw(1).withdraw(1).yield_interest().display_account_info()
+
+
+
+
+
+
+
+
+
+# tomas = User('Tomas', 'email')
+# kana = User('Kana', 'email1')
+# graham = User('Gram', 'email2')
+
+# tomas.make_deposit(1000000).make_deposit(1000000).make_deposit(1000000).make_withdrawal(1).display_account_balance()
+
+# kana.make_deposit(1000000).make_deposit(1000000).make_withdrawal(1).make_withdrawal(1).display_account_balance()
+
+# graham.make_deposit(1000000).make_deposit(1000000).make_withdrawal(1).make_withdrawal(1).display_account_balance()
